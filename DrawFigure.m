25fb940a598259479c712832e5311dacf77059b4
PatrickRen <renqs@163.com>
2017-03-29 23:40:20 +0800

    Move DrawFigure.m (MATLAB) to the repo to manage in Git

---
+++ DrawFigure.m
@@ -0,0 +1,61 @@
+function DrawFigure()
+DataHys = load('d:\\hysdata.txt');
+TimeHys = DataHys(:,1);
+CurrentHys = DataHys(:,2);
+SpeedHys = DataHys(:,3);
+IStateHys = DataHys(:,4);
+%SStateHys = DataHys(:,5);
+IGBTHys = DataHys(:,6);
+
+DataPID = load('d:\\piddata.txt');
+TimePID = DataPID(:,1);
+CurrentPID = DataPID(:,2);
+SpeedPID = DataPID(:,3);
+IStatePID = DataPID(:,4);
+%SStatePID = DataPID(:,5);
+IGBTPID = DataPID(:,6);
+
+figure(1);
+subplot(2,1,1);
+[AX,~,~] = plotyy(TimePID,CurrentPID,TimePID,IStatePID,'plot');
+%[AX,~,~] = plotyy(TimePID(15000:25000),CurrentPID(15000:25000),TimePID(15000:25000),IStatePID(15000:25000),'plot');
+title('Q-Winding Current');
+axis(AX(1),[-Inf,Inf,-Inf,16]);
+%axis(AX(1),[0.15,0.25,-Inf,16]);
+axis(AX(2),[-Inf,Inf,0,5]);
+%axis(AX(2),[0.15,0.25,0,5]);
+set(AX(1),'ytick',[0,14,15]);
+set(AX(2),'ytick',[0,1]);
+xlabel('Time / s');
+ylabel(AX(1),'Iq / A');
+ylabel(AX(2),'Current Controller');
+hold on;
+plot(TimeHys,CurrentHys);
+plot([0,TimeHys(end)],[15,15],'Color',[180 180 180]/255);
+plot([0,TimeHys(end)],[14,14],'Color',[180 180 180]/255);
+%hold off;
+%legend('PID Controller','Hysteresis Controller');
+
+subplot(2,1,2);
+[AX,~,~] = plotyy(TimePID,SpeedPID,TimePID,IGBTPID,'plot');
+%[AX,~,~] = plotyy(TimePID(15000:25000),SpeedPID(15000:25000),TimePID(15000:25000),IGBTPID(15000:25000),'plot');
+title('Rotating Speed');
+axis(AX(1),[-Inf,Inf,0,140]);
+%axis(AX(1),[0.15,0.25,0,140]);
+axis(AX(2),[-Inf,Inf,0,5]);
+%axis(AX(2),[0.15,0.25,0,5]);
+set(AX(1),'ytick',[0,80,120]);
+set(AX(2),'ytick',[0,1]);
+xlabel('Time / s');
+ylabel(AX(1),'Omega / (rad/s)');
+ylabel(AX(2),'Controller');
+hold on;
+plot(TimeHys,SpeedHys);
+plot([0,TimeHys(end)],[80,80],'Color',[180 180 180]/255);
+% plot([0,TimeHys(end)],[82,82],'r');
+% plot([0,TimeHys(end)],[78,78],'r');
+plot([0,TimeHys(end)],[120,120],'Color',[180 180 180]/255);
+% plot([0,TimeHys(end)],[122,122],'r');
+% plot([0,TimeHys(end)],[118,118],'r');
+%hold off;
+end
